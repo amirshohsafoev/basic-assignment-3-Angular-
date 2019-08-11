f08207d8f34346c1ef2c1ae9169181eddd479078
amirshohsafoev <amirsafoev0@gmail.com>
2019-08-11 18:55:17 -0400

    *ngIf and *ngFor are strcutural directives. ngClass and ngStyle are attribute directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'basic-assignment-three';
+  visiblity = "hidden";
+  count = [0];
+  change = "";
+
+  getColor() {
+    if (this.count[this.count.length - 1] > 5) {
+      this.change = "super";
+      return "blue";
+    }
+  }
+
+  onDisplay() {
+    this.visiblity === "hidden"
+      ? (this.visiblity = "inline")
+      : (this.visiblity = "hidden");
+    this.count.push(this.count[this.count.length - 1] + 1);
+    console.log(this.count);
+  }
 }
